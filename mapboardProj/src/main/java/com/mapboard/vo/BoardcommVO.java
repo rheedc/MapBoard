3999df466933f00442ead0a8ac8ea9874375d872
hong <hong@302-07>
2018-12-07 20:19:02 +0900

    VO클래스 commit시 꼭 송승환에게 얘기하시길...

---
+++ mapboardProj/src/main/java/com/mapboard/vo/BoardcommVO.java
@@ -0,0 +1,17 @@
+package com.mapboard.vo;
+
+public class BoardcommVO extends CommonVO{
+	
+	private int bcidx;
+
+	public int getBcidx() {
+		return bcidx;
+	}
+
+	public void setBcidx(int bcidx) {
+		this.bcidx = bcidx;
+	}
+	
+	
+
+}
---
+++ mapboardProj/src/main/java/com/mapboard/vo/PlaceVO.java
@@ -0,0 +1,43 @@
+package com.mapboard.vo;
+
+public class PlaceVO extends CommonVO{
+	private String place_name;
+	private int category_no;
+	private int sigungu_code;
+	private String sigungu_name;
+	private String doro_juso;
+	
+	
+	
+	public String getPlace_name() {
+		return place_name;
+	}
+	public void setPlace_name(String place_name) {
+		this.place_name = place_name;
+	}
+	public int getCategory_no() {
+		return category_no;
+	}
+	public void setCategory_no(int category_no) {
+		this.category_no = category_no;
+	}
+	public int getSigungu_code() {
+		return sigungu_code;
+	}
+	public void setSigungu_code(int sigungu_code) {
+		this.sigungu_code = sigungu_code;
+	}
+	public String getSigungu_name() {
+		return sigungu_name;
+	}
+	public void setSigungu_name(String sigungu_name) {
+		this.sigungu_name = sigungu_name;
+	}
+	public String getDoro_juso() {
+		return doro_juso;
+	}
+	public void setDoro_juso(String doro_juso) {
+		this.doro_juso = doro_juso;
+	}
+
+}
